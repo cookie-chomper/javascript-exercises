f9bdbaf670575956dbbd90e9a30d5386a5a9577e
Karol <kstachowicz@icloud.com>
2022-04-25 20:40:27 +0200

    solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    let finalString = '';
+    if (n < 0) return 'ERROR';
+    for (let i=0; i<n; i++) {
+        finalString = finalString + string;
+    };
+    return finalString;
 };
 
 // Do not edit below this line
