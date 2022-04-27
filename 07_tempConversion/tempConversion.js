ac2a7ec7b2190d1d089a3f1e82d9a7d572a92c84
Karol <kstachowicz@icloud.com>
2022-04-27 12:17:59 +0200

    solve exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+    ctemp = (ftemp - 32) * 0.5556;
+    ctemp = parseFloat(ctemp.toFixed(1));
+    return ctemp;
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+    ftemp = ctemp * 1.8 + 32;
+    ftemp = parseFloat(ftemp.toFixed(1));
+    return ftemp;
 };
 
 // Do not edit below this line
