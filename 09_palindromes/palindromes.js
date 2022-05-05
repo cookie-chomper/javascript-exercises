1ba382495da79e76f32494f87fba8477b5ac6458
Karol <kstachowicz@icloud.com>
2022-05-05 20:47:20 +0200

    solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (stringA) {
+    stringA = stringA.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let temp = [];
+    for (let i = 0; i < stringA.length; i++) {
+        temp.push(stringA[stringA.length - 1 - i]);
+    }
+    temp = temp.join('');
+    console.log(stringA);
+    console.log(temp);
+    let bool = (temp == stringA) ? true : false;
+    return bool;
 
-};
+    }
 
-// Do not edit below this line
+//console.log(palindromes('Rac,e ca.r'));
+// Do not edit below this  
 module.exports = palindromes;
