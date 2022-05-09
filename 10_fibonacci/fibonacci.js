79549ea0b38b58a5f0b6963ed1d0af28c16f470b
Karol <kstachowicz@icloud.com>
2022-05-09 14:30:49 +0200

    solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n <= 2) {
+        if (n < 0) {
+            return "OOPS";
+        }
+        return 1;
+    }
+    else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
 };
 
 // Do not edit below this line
