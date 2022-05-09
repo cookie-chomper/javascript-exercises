cb11a5e588363bc253eb354de8161d8f1d96e9c1
Karol <kstachowicz@icloud.com>
2022-05-09 15:30:24 +0200

    solve exercises 11 & 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArray) {
+    let titles = [];
+    for (let i = 0; i < bookArray.length; i++) {
+        let title = bookArray[i].title;
+        titles.push(title);
+
+    }
+    return titles;
 
 };
 
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
-};
-
+    const date = Date.now();
+    const dateInYears = Math.floor(1970 + date/1000/60/60/24/365);
+    console.log(date);
+    let tempAge = 0;
+    let oldest;
+    let age;
+    for (person of people) {
+        //check if person still alive
+        if (person.yearOfDeath) {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        //if still alive:
+        else {
+            age = dateInYears - person.yearOfBirth;
+        }
+        if (age > tempAge) {
+            tempAge = age;
+            oldest = person
+            } 
+        }
+    return oldest;
+    };
 // Do not edit below this line
 module.exports = findTheOldest;
